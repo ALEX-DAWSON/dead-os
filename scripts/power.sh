30af6f12622682389acd62d8b25284be878d1607
ALEX-DAWSON <ktad2001@gmail.com>
2025-09-22 18:25:07 -0400

    adding power menu button to waybar

--- scripts/power.sh
+++ scripts/power.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown\nx Cancel"
+entries="󰌾  Lock\n󰍃  Logout\n󰒲  Sleep\n󰑓  Reboot\n󰐥  Shutdown\n󰜺 Cancel"
 
-selected=$(echo -e $entries|wofi --lines 6 -c ~/.config/wofi/config-power | awk '{print tolower($2)}')
+selected=$(echo -e $entries|wofi --lines 7 -c ~/.config/wofi/config-power | awk '{print tolower($2)}')
 
 case $selected in
+  lock)
+    sleep 1
+    hyprlock
+  ;;
   logout)
-    hyprctl dispatch exit;;
-  suspend)
-    exec systemctl suspend;;
+    hyprctl dispatch exit
+  ;;
+  sleep)
+    exec systemctl suspend
+  ;;
   reboot)
-    exec systemctl reboot;;
+    exec systemctl reboot
+  ;;
   shutdown)
-    exec systemctl poweroff -i;;
+    exec systemctl poweroff -i
+  ;;
   cancel)
     exit
   ;;
