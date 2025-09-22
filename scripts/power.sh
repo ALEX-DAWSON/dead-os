22a7ab00b886fd99499ee613c871fe6f480de686
ALEX-DAWSON <ktad2001@gmail.com>
2025-09-22 00:17:08 -0400

    added cancel option

--- scripts/power.sh
+++ scripts/power.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown\nx Cancel"
 
-selected=$(echo -e $entries|wofi -c ~/.config/wofi/config-power | awk '{print tolower($2)}')
+selected=$(echo -e $entries|wofi --lines 6 -c ~/.config/wofi/config-power | awk '{print tolower($2)}')
 
 case $selected in
   logout)
-    exit;;
+    hyprctl dispatch exit;;
   suspend)
     exec systemctl suspend;;
   reboot)
     exec systemctl reboot;;
   shutdown)
     exec systemctl poweroff -i;;
+  cancel)
+    exec exit
+  ;;
 esac
